2c5aa852719e232bf3c9ae0ea0a8e955e26124fd
ubiracidomingosoliveira <ubiracidomingosoliveira@gmail.com>
2024-12-08 21:26:18 -0300

    [Ubiraci] Corrigido erro ao enviar

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,5 +1,4 @@
-import { cwd } from "process";
-import {getTodosposts, criarPost } from "../models/postsModels.js";
+/* import {getTodosposts, criarPost } from "../models/postsModels.js";
 import fs from "fs";
 
 export async function listarPosts(req, res)
@@ -30,7 +29,7 @@ export async function uploadImagem(req, res) {
 
    try {
       const postCriado = await criarPost(novoPost);
-      const imagemAtualizada = 'uploads/${postCriado.insertdId}.png'
+      const imagemAtualizada = 'uploads/${postCriado.insertedId}.png'
       fs.renameSync(req.file.path, imagemAtualizada)
       req.status(200).json(postCriado);
         
@@ -40,7 +39,46 @@ export async function uploadImagem(req, res) {
    }
    
 }
+ */
+//======================Teste=================================================================================
+//import {getTodosPosts, criarPost} from "../models/postsModel.js";
+import {getTodosposts, criarPost } from "../models/postsModels.js";
+import fs from "fs";
 
+export async function listarPosts(req, res) {
+    // Chama a função para buscar os posts
+    const posts = await getTodosPosts();
+    // Envia uma resposta HTTP com status 200 (OK) e os posts no formato JSON
+    res.status(200).json(posts);
+}
 
+export async function postarNovoPost(req, res) {
+    const novoPost = req.body;
+    try {
+        const postCriado = await criarPost(novoPost);
+        res.status(200).json(postCriado);  
+    } catch(erro) {
+        console.error(erro.message);
+        res.status(500).json({"Erro":"Falha na requisição"})
+    }
+}
+
+export async function uploadImagem(req, res) {
+    const novoPost = {
+        descricao: "",
+        imgUrl: req.file.originalname,
+        alt: ""
+    };
+
+    try {
+        const postCriado = await criarPost(novoPost);
+        const imagemAtualizada = `uploads/${postCriado.insertedId}.png`
+        fs.renameSync(req.file.path, imagemAtualizada)
+        res.status(200).json(postCriado);  
+    } catch(erro) {
+        console.error(erro.message);
+        res.status(500).json({"Erro":"Falha na requisição"})
+    }
+}
 
    
